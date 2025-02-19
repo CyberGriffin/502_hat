27ee4d6c787bc9027416bbe8450b78687c03ba39
jinyuan0425 <pdmon123@gmail.com>
2025-02-19 16:13:52 -0600

    Add code cov to repo

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,18 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/items/new"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do 
+    it "creates an item successfully" do
+      post items_path, params: { item: { name: "Test Item", description: "Test description", price: 10 } }
+      expect(response).to have_http_status(302)
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/items/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
