b0d3f137ba89a5fdeb5e6b103b66730035385ab3
jinyuan0425 <pdmon123@gmail.com>
2025-03-06 12:04:47 -0600

    Update seed_whitelists.rb

--- config/initializers/seed_whitelists.rb
+++ config/initializers/seed_whitelists.rb
@@ -1,11 +1,11 @@
 Rails.application.config.after_initialize do
     if ActiveRecord::Base.connection.table_exists?('whitelisted_emails')
-      emails = [
-        "kwohjinyuan@tamu.edu",
-        "griffinbeaudreau@tamu.edu",
-        "nicholasmatias@tamu.edu",
-        "suresh06192004@tamu.edu",
-        "james.reyes@tamu.edu"
+      whitelists = [
+        { email: "kwohjinyuan@tamu.edu", expires_at: nil }
+        { email: "griffinbeaudreau@tamu.edu", expires_at: nil }
+        { email: "nicholasmatias@tamu.edu", expires_at: nil }
+        { email: "suresh06192004@tamu.edu", expires_at: nil }
+        { email: "james.reyes@tamu.edu", expires_at: nil }
       ]
   
       emails.each do |email|
