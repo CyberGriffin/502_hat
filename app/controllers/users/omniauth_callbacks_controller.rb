bbab5e099582bbde9ebadae69088cd56b22ae569
CyberGriffin <griffinbeaudreau@gmail.com>
2025-04-06 19:52:20 -0500

    Fixed issue with flash messages being handled at too high of a level, causing issues with styling

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,8 +8,7 @@ module Users
                     # flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
                     sign_in_and_redirect user, event: :authentication
                else
-                    # flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google',
-                    #                                                        reason: "#{auth.info.email} is not authorized. Please use your TAMU issued email."
+                    flash[:alert] = "#{auth.info.email} is not authorized. Please use your TAMU issued email."
                     redirect_to new_user_session_path
                end
           end
