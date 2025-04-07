1116c115f5fc69f7a481ebd5e3fada86d92c9c26
CyberGriffin <griffinbeaudreau@gmail.com>
2025-04-07 14:25:20 -0500

    Added filter

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link application.js
 //= link inventories.js
-//= link search.js
+//= link search-filter.js
 //= link sorting.js
 //= link inventory-edit.js
