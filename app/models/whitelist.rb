7d3aedb2d8bea31e05087dad7b8389b852f10a5e
jinyuan0425 <pdmon123@gmail.com>
2025-04-07 23:32:04 -0500

    Test whitelist

--- app/models/whitelist.rb
+++ app/models/whitelist.rb
@@ -4,10 +4,4 @@ class Whitelist < ApplicationRecord
   def expired?
     expires_at.present? && expires_at < Date.today
   end
-
-  private
-
-  def set_default_role
-    self.roles ||= 'user'
-  end
 end
