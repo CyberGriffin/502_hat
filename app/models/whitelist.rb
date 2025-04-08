60ddb88f3635796e024c2c64fc2f64f2d4c76783
jinyuan0425 <pdmon123@gmail.com>
2025-04-07 23:17:28 -0500

    Test whitelist

--- app/models/whitelist.rb
+++ app/models/whitelist.rb
@@ -1,10 +1,8 @@
 class Whitelist < ApplicationRecord
      validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
    
-     validates :roles, inclusion: { in: ['user', 'admin'], message: "%{value} is not a valid role" }
-   
-     validate :validate_role
-   
+     enum roles: { user: 'user', admin: 'admin' }
+     
      def expired?
        expires_at.present? && expires_at < Date.today
      end
@@ -13,13 +11,6 @@ class Whitelist < ApplicationRecord
    
      def set_default_role
        self.roles ||= 'user'
-     end
-
-     def validate_role
-      valid_roles = ['user', 'admin']
-      unless valid_roles.include?(roles)
-        errors.add(:roles, "#{roles} is not a valid role")
-      end
      end
    end
    
