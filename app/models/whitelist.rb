9ffe73ac9050fa382a304ccfb7a90d1f7014780c
jinyuan0425 <pdmon123@gmail.com>
2025-04-07 23:25:16 -0500

    Test Whitelist

--- app/models/whitelist.rb
+++ app/models/whitelist.rb
@@ -1,16 +1,13 @@
 class Whitelist < ApplicationRecord
-     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-   
-     enum roles: { user: 'user', admin: 'admin' }
-     
-     def expired?
-       expires_at.present? && expires_at < Date.today
-     end
-   
-     private
-   
-     def set_default_role
-       self.roles ||= 'user'
-     end
-   end
-   
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def expired?
+    expires_at.present? && expires_at < Date.today
+  end
+
+  private
+
+  def set_default_role
+    self.roles ||= 'user'
+  end
+end
